713629ef3eccd6bca5772b1e623598466c840f25
NinerMG <maciej.gala@hotmail.com>
2022-05-19 10:07:04 +0200

    Initial commit

---
+++ src/mg/FindAlgorithm.java
@@ -0,0 +1,13 @@
+package mg;
+
+import java.io.IOException;
+
+public class FindAlgorithm {
+
+    public void find() throws IOException {
+        Intialize intialize = new Intialize();
+        intialize.fileToArray();
+
+    }
+
+}
---
+++ src/mg/Main.java
@@ -0,0 +1,16 @@
+package mg;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        // write your code here
+        FindAlgorithm findAlgorithm = new FindAlgorithm();
+        findAlgorithm.find();
+
+    }
+}
+
